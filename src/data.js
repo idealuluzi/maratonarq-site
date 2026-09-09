67975baf9cf4135b29a2e5184091ba68f9c1a95f
Kissiinii <lucasqm2805@gmail.com>
2026-09-09 15:40:22 -0300

    Remove item de orientações do hackathon da programação

--- src/data.js
+++ src/data.js
@@ -134,7 +134,6 @@ export const DATA = {
         { t: '11h30', title: 'Contextualização + tema', kind: 'hackathon', track: 0 },
         { t: '12h30', title: 'Almoço', kind: 'evento' },
         { t: '14h', title: 'REDRAW', kind: 'oficina' },
-        { t: '14h30', title: 'Orientações do hackathon', kind: 'hackathon', track: 0, revealed: false },
         { t: '14h30', title: 'Doppio', kind: 'palestra', track: 1 },
         { t: '15h30', title: 'Confraternização | Coffee break', kind: 'evento' },
         { t: '16h', title: 'Laís Preisler', kind: 'palestra', track: 1 },
